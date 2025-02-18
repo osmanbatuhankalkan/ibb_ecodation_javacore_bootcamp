1f53d77fc408215b20688376511311318db87fa9
Osman Batuhan Kalkan <osmankalkan21@istanbularel.edu.tr>
2025-02-18 18:50:11 +0300

    .

---
+++ src/main/java/com/osmanbatuhankalkan/_3_week/faktoriyel_SC.java
@@ -0,0 +1,19 @@
+package com.osmanbatuhankalkan._3_week;
+
+import java.util.Scanner;
+
+public class faktoriyel_SC {
+    public static void main(String[] args) {
+        int faktoriyelDegeri;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bulmak istediğiniz, Faktoriyel değerini giriniz: ");
+        faktoriyelDegeri = input.nextInt();
+        int faktoriyelSonuc=1;
+        for (int i = 1; i <= faktoriyelDegeri; i++) {
+            faktoriyelSonuc=(faktoriyelSonuc*i);
+            System.out.println("Faktoriyel: " + faktoriyelSonuc);
+
+        }
+    }
+
+}
